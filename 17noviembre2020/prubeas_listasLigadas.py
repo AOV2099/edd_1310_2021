82964100f5aa9ef3377d8bb24e1a82df7799ea18
AOV2099 <71807776+AOV2099@users.noreply.github.com>
2020-11-17 13:08:36 -0600

    Add files via upload

---
+++ 17noviembre2020/prubeas_listasLigadas.py
@@ -0,0 +1,13 @@
+from listas import LinkedList
+
+l = LinkedList()
+print(f"l está vacía?, { l.is_empty() }")
+
+l.append(10)
+l.append(5)
+l.append(6)
+l.append(20)
+
+print(f"l está vacía?, { l.is_empty() }")
+
+l.transversal()
